181271d8ed5bad31078180817c68ed53c713afc4
yNeshy <zizou.nechi@gmail.com>
2020-11-06 13:02:09 +0100

    Limited depth search and random moves search

--- game.py
+++ game.py
@@ -2,34 +2,8 @@ from copy import copy
 from time import sleep
 from random import shuffle
 import os
-from players import CLIPlayer
-
-END = 0
-TEMPLATE = """
-  _________________
- |     |     |     |
- |  (  |  é  |  "  |
- |_____|_____|_____|
- |     |     |     |
- |  @  |  &  |  ç  |
- |_____|_____|_____|
- |     |     |     |
- |  -  |  è  |  )  |
- |_____|_____|_____|
- """
-    
-    # î
-    # | columns and rows identifiers.
-
-TEMPLATE_LIST = ['(','é','"','@','&','ç','-','è',')']
-
-BOARD = [' ', '1', '2', '3', '4', '5', '6', '7', '8'] # initial board
-
-CONFIG = {
-    'rows':3,
-    'cols':3,
-    'sir':'Undefined',
-}
+from players import CLIPlayer, RandomPlayer, AstarAlgorithm, LimitedDepthPlayer
+import time
 
 
 class TemplateTaquinPlayer :
@@ -40,81 +14,137 @@ class TemplateTaquinPlayer :
     def move(self, board, config):
         return piece_to_move
 
+class CLITaquin():
+    def __init__(self):
+        self.player = LimitedDepthPlayer()
+        self.player.set_max_depth(34)
+        
+        
+        self.END = 0
+        self.TEMPLATE = """
+        _________________
+        |     |     |     |
+        |  (  |  é  |  "  |
+        |_____|_____|_____|
+        |     |     |     |
+        |  @  |  &  |  ç  |
+        |_____|_____|_____|
+        |     |     |     |
+        |  -  |  è  |  )  |
+        |_____|_____|_____|
+        """  
+            # î
+            # | columns and rows identifiers.
+        self.TEMPLATE_LIST = ['(','é','"','@','&','ç','-','è',')']
+        self.BOARD = [' ', '1', '2', '3', '4', '5', '6', '7', '8'] # initial board
+        self.CONFIG = {
+            'rows':3,
+            'cols':3,
+            'sir':'Undefined'}
+        self.ROUND = 0
 
+    def clear(self):
+        # for mac and linux(here, os.name is 'posix')
+        if os.name == 'posix':
+            _ = os.system('clear')
+        else:
+            # for windows platfrom
+            _ = os.system('cls')
 
-player = CLIPlayer()
+    def is_empty(self, case):
+        """ return if a case is empty the board """
+        if (case < 0):
+            return False
+        if (case >= len(BOARD)):
+            return False
+        if (BOARD[case] != ' '):
+            return False
+        return True
 
-def clear():
-   # for mac and linux(here, os.name is 'posix')
-   if os.name == 'posix':
-      _ = os.system('clear')
-   else:
-      # for windows platfrom
-      _ = os.system('cls')
+    def draw(self):
+        curr_BOARD = copy(self.TEMPLATE)
+        for i in range(len(self.BOARD)):
+            curr_BOARD = curr_BOARD.replace(self.TEMPLATE_LIST[i], self.BOARD[i])
+        print("sir {} is playing. Do not disturb.".format(self.CONFIG['sir']))
+        print("Round: "+str(self.ROUND))
+        print(curr_BOARD)
+        del curr_BOARD
+    
+    def update(self):
+        self.clear()
+        self.draw()
+        x = self.player.move(self.BOARD, self.CONFIG)
+        print("Sir {} asked to move {}.".format(self.CONFIG['sir'], x))
+        input()
+        if (self.is_valid(x)):
+            space = self.BOARD.index(' ')
+            new_space = self.BOARD.index(x)
+            self.BOARD[space] , self.BOARD[new_space] = self.BOARD[new_space] , self.BOARD[space]
+            if self.is_won():
+                self.END=1
+        else :
+            self.END=-1
 
-def is_empty(case):
-    """ return if a case is empty the board """
-    if (case < 0):
-        return False
-    if (case >= len(BOARD)):
-        return False
-    if (BOARD[case] != ' '):
-        return False
-    return True
+    def is_won(self):
+        won = True
+        i = 0
+        while (won &
+         (i< (len(self.BOARD) -1))
+         ):
+            i = i+1
+            won = (self.BOARD[i-1] <= self.BOARD[i])
+        return won
 
-def draw():
-    curr_BOARD = copy(TEMPLATE)
-    for i in range(len(BOARD)):
-        curr_BOARD = curr_BOARD.replace(TEMPLATE_LIST[i], BOARD[i])
-    print("sir {} is playing. Do not disturb.".format(CONFIG['sir']))
-    print(curr_BOARD)
+    def setup(self):
+        shuffle(self.BOARD)
+        
+        self.ROUND = 0
+        self.END = 0
+        #print("How should I call you fine sir?: ")
+        no_name = True
+        while(no_name):
+            self.CONFIG['sir'] = "algorithm"  #str(input()).capitalize()
+            no_name = ( len(self.CONFIG['sir']) == 0 )
+        print("Setup complete.")
+        print(self.CONFIG['sir'] + ", let us begin.")
 
-def update():
-    clear()
-    draw()
-    x = player.move(BOARD, CONFIG)
-    if (is_valid(x)):
-        space = BOARD.index(' ')
-        new_space = BOARD.index(x)
-        BOARD[space] , BOARD[new_space] = BOARD[new_space] , BOARD[space]
-    else :
-        END=-1
+    def is_valid(self, x):
+        # x being the piece we want to move
+        tmp = []
+        empty = self.BOARD.index(' ')
+        upper = empty - int(self.CONFIG['cols'])
+        lower = empty + int(self.CONFIG['cols'])
+        tmp.append(upper)
+        tmp.append(lower)
+        if( ( empty  % int(self.CONFIG['cols']) != 0 )):
+            left = empty - 1
+            tmp.append(left)
+        if( ((empty-1)%int(self.CONFIG['cols']) != 0 )):
+            right = empty + 1
+            tmp.append(right)
+        valid_moves = [self.BOARD[i] for i in tmp if i in range(int(self.CONFIG['cols']) * int(self.CONFIG['rows'])) ]
+        
+        return (x in valid_moves)
 
-def setup():
-    shuffle(BOARD)
-    END = 0
-    print("How should I call you fine sir?: ")
-    no_name = True
-    while(no_name):
-        CONFIG['sir'] = str(input()).capitalize()
-        no_name = ( len(CONFIG['sir']) == 0 )
-    print("Setup complete.")
-    print(CONFIG['sir'] + ", let us begin.")
+    def main(self):
+        print("Enter to enter.")
+        while(str(input()) == ''):
+            self.setup()
+            self.draw()
+            print("Enter a character to begin.")
 
-def is_valid(x):
-    # x being the piece we want to move
-    empty = BOARD.index(' ')
-    new = BOARD.index(x)
-    upper = empty - int(CONFIG['cols'])
-    lower = empty + int(CONFIG['cols'])
-    left = empty - 1
-    right = empty + 1
-    valid_moves = [upper, lower, left, right]
-    return (new in valid_moves)
+        sleep(1)  
+        while(self.END==0):
+            self.ROUND += 1
+            self.update()
+            sleep(0.5)
 
-def main():
-    setup()
-    sleep(1)
-    while(END==0):
-        update()
-        sleep(3)
-
-    if (END == -1):
-        print("Pathetic.")
-
-    else :
-        print("Decent enough. I will grant you passage.")
+        if (self.END == -1):
+            print("Pathetic.")
+        else :
+            self.draw()
+            print("Decent enough. I will grant you passage.")
 
 
 if __name__ == "__main__":
-    main()
+    CLITaquin().main()
--- players.py
+++ players.py
@@ -1,33 +1,198 @@
 from random import randrange
+from copy import copy
+import queue
+import time
 
-#class GeneticAlgorithm():
+class AstarAlgorithm():
+    def __init__(self):
+        self.solved = False
+        self.previous_move = ''
 
+    def distance_de_manhattan(self, xA, yA, xB, yB ):
+        return abs(xB-xA) + abs(yB-yA)
 
+    def possible_moves(self):
+        tmp = []
+        empty = self.board.index(0)
+        upper = empty - int(self.config['cols'])
+        lower = empty + int(self.config['cols'])
+        tmp.append(upper)
+        tmp.append(lower)
+        if( ( empty  % int(self.config['cols']) != 0 )):
+            left = empty - 1
+            tmp.append(left)
+        if( ((empty-1)%int(self.config['cols']) != 0 )):
+            right = empty + 1
+            tmp.append(right)
+        valid_moves = [self.board[i] for i in tmp if i in range(int(self.config['cols']) * int(self.config['rows'])) ]
+        return valid_moves
 
+    def heuristic(self, board):
+        total_manhattan = 0
+        for i in range(len(board)):
+            xA = int(board[i])/3
+            yA = int(board[i])%3
+            xB = i/3
+            yB = i%3
+            total_manhattan += self.distance_de_manhattan(xA, yA, xB, yB)
+        return total_manhattan
 
+    def move(self, board, config):
+        self.board = board
+        self.config = config
+        options = self.possible_moves()
+        min_heuristic = 9**9    # worst case scenario
+        final_move = None
+        for move in possible_moves():
+            if (move != self.previous_move):
+                local_board = copy(board)
+                _out = self.BOARD.index(0)
+                _in = self.BOARD.index(move)
+                
+                local_board[_out] , local_board[_in] = local_board[_in] , local_board[_out]
+                curr_heuristic = self.heuristic(local_board)
+                if(curr_heuristic <= min_heuristic):
+                    min_heuristic = curr_heuristic
+                    final_move = move
+        self.previous_move = final_move
+        return final_move
 
 # implement move(board, config)
 class RandomPlayer():
-    def possible_moves_for_x(self, x):
-        upper = x - config['cols']
-        lower = x + config['cols']
-        left = x - 1
-        right = x + 1
-        result = []
-        if (empty(upper) ): result.append(upper)
-        if (empty(lower) ): result.append(lower)
-        if (empty(left) ): result.append(left)
-        if (empty(right) ): result.append(right)
-        return result 
+
+    def possible_moves(self, board, config):   
+        tmp = []
+        empty = board.index(' ')
+        upper = empty - int(config['cols'])
+        lower = empty + int(config['cols'])
+        tmp.append(upper)
+        tmp.append(lower)
+        if( ( empty  % int(config['cols']) != 0 )):
+            left = empty - 1
+            tmp.append(left)
+        if( ((empty-1)%int(config['cols']) != 0 )):
+            right = empty + 1
+            tmp.append(right)
+        valid_moves = [board[i] for i in tmp if i in range(int(config['cols']) * int(config['rows'])) ]
+        return valid_moves
 
     def move(self, board, config):
+        allowed_moves = self.possible_moves(board , config)
         move = allowed_moves[randrange(len(allowed_moves))]
         return (move)
 
-
 class CLIPlayer():
-
     def move(self, board, config):
         piece = int(input("Piece to move: "))
         return str(piece)
 
+class LimitedDepthPlayer():
+
+    def __init__(self):
+        self.max_depth = 5 # experiment with this here
+        self.solved = False
+        self.searched_once = False
+        self.solution = []
+
+    def set_max_depth(self, depth):
+        self.max_depth = depth
+
+    def wins(self, board):
+        won = True
+        i = 0
+        while (won &(i< (len(board) -1))):
+            i = i+1
+            won = (int(board[i-1]) <= int(board[i]))
+        return won
+
+    def __possible_moves(self, board):   
+        tmp = []
+        empty = board.index(0)
+        upper = empty - int(self.config['cols'])
+        lower = empty + int(self.config['cols'])
+        tmp.append(upper)
+        tmp.append(lower)
+        if( ( empty  % int(self.config['cols']) != 0 )):
+            left = empty - 1
+            tmp.append(left)
+        if( ((empty-1)%int(self.config['cols']) != 0 )):
+            right = empty + 1
+            tmp.append(right)
+        valid_moves = [int(board[i]) for i in tmp if i in range(int(self.config['cols']) * int(self.config['rows'])) ]
+        return valid_moves
+
+    def __simulate_move(self, move, board):
+        new_board = copy(board)
+        old_0 = new_board.index(0)
+        new_0 = new_board.index(int(move))
+        new_board[old_0], new_board[new_0] = new_board[new_0], new_board[old_0]
+        return new_board
+
+    def __search(self, board, trace, depth, previous_move):
+        if (self.wins(board)):
+            self.solution = trace
+            self.solution.reverse()
+            self.solved = True
+            self.searched_once = True
+            print(1/0)
+            return
+
+        if(depth >= self.max_depth):
+            return False
+        children = [ self.__simulate_move(move, board) for move in self.__possible_moves(board) ]
+
+        for move in self.__possible_moves(board) :
+            if(move != previous_move) :    
+                curr_board = self.__simulate_move(move, board)
+                trace.append(move)
+                self.__search(curr_board, trace, depth+1, move)
+                trace.remove(move)
+        return
+
+    def move(self, board, config):
+        if(self.wins(board)):
+            return board[0]
+        print("My max depth is : "+str(self.max_depth))
+        if not self.searched_once :
+            print("Calculating...")
+            self.board = copy(board)
+            self.board[self.board.index(' ')] = 0
+            self.board = [ int(ele) for ele in self.board ]
+            self.config = config
+            try:
+                start_time = time.time()
+                self.__search(self.board, [], 0, 0)
+                print("--- %s seconds ---" % (time.time() - start_time))
+                self.searched_once = True
+            except(ZeroDivisionError):
+                print("SOLVED")
+                self.solved = True
+                self.searched_once = True
+                
+        if self.solved:
+            return str(self.solution.pop() )  # no i want the other extremity hon
+        else :
+            print("Failed to find a solution in reasonable time.")
+            input()
+            return '3'
+
+
+if __name__ == "__main__":
+    debug_board = ['1',' ','2','3','4','5','6','7','8']
+    debug_config={
+        'rows':3,
+        'cols':3,
+        'sir':'Undefined',}
+
+    p = LimitedDepthPlayer()
+    p.move(debug_board, debug_config)
+        # _________________
+        # |     |     |     |
+        # |  1  |     |  2  |
+        # |_____|_____|_____|
+        # |     |     |     |
+        # |  3  |  4  |  5  |
+        # |_____|_____|_____|
+        # |     |     |     |
+        # |  6  |  7  |  8  |
+        # |_____|_____|_____|
